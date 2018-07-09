c851d37a5cc76b436fc65c5f158447b6db5ddb71
Jason Shin <visualbbasic@gmail.com>
2018-07-09 19:57:47 +1000

    word2vec: implementing model compile

--- 032-word2vec/model.py
+++ 032-word2vec/model.py
@@ -1,10 +1,13 @@
 import tensorflow as tf
 
+
 class Word2vec:
   batch_size = 20
   embedding_size = 2
   num_sampled = 15 # Number of negative examples to sample.
   vocab_size = 0
+  loss = None
+  optimizer = None
 
   def __init__(
       self,
@@ -22,16 +25,21 @@ class Word2vec:
     self.X = tf.placeholder(tf.int32, shape=[batch_size])
     self.y = tf.placeholder(tf.int32, shape=[batch_size, 1])
 
+  def compile(self):
     # Look up embeddings for inputs.
     # The conversion of 10,000 columned matrix into a 200 columned matrix is called word embedding.
-    embeddings = tf.Variable(tf.random_uniform([self.vocab_size, embedding_size], -1.0, 1.0))
+    embeddings = tf.Variable(
+      tf.random_uniform([self.vocab_size, self.embedding_size], -1.0, 1.0))
     embed = tf.nn.embedding_lookup(embeddings, self.X)  # lookup table
 
     # Construct the variables for the NCE loss
-    nce_weights = tf.Variable(tf.random_uniform([self.vocab_size, embedding_size], -1.0, 1.0))
+    nce_weights = tf.Variable(
+      tf.random_uniform([self.vocab_size, self.embedding_size], -1.0, 1.0))
     nce_biases = tf.Variable(tf.zeros([self.vocab_size]))
 
     # Compute the average NCE loss for the batch.
-    loss = tf.reduce_mean(tf.nn.nce_loss(nce_weights, nce_biases, self.Y, embed, self.num_sampled, self.vocab_size))
+    self.loss = tf.reduce_mean(
+      tf.nn.nce_loss(nce_weights, nce_biases, self.Y, embed, self.num_sampled,
+                     self.vocab_size))
     # Using the adam optimizer
-    optimizer = tf.train.AdamOptimizer(1e-1).minimize(loss)
+    self.optimizer = tf.train.AdamOptimizer(1e-1).minimize(loss)
